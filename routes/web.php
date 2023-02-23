414fc45d40e672a34fbda51613e72000704f7dbe
Carlos Obedé <carlosobede@gmail.com>
2023-02-23 17:03:51 +0100

    commit examen

--- routes/web.php
+++ routes/web.php
@@ -125,10 +125,10 @@ Route::resource("studies", StudyController::class);
 
 
 
-//Auth::routes();
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// //Auth::routes();
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
