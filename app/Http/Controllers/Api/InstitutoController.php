be498a9c67067b420006cb5620c666aafb5e18b4
Carlos Obedé <carlosobede@gmail.com>
2023-02-27 16:48:40 +0100

    commit

--- app/Http/Controllers/Api/InstitutoController.php
+++ app/Http/Controllers/Api/InstitutoController.php
@@ -8,15 +8,7 @@ use Illuminate\Http\Request;
 
 class InstitutoController extends Controller
 {
-     /* Funcion por si queremos añadir en cada función la verificación, para no tener que copiar y pegar el mismo codigo todo el rato
-    public function check404($study)
-    {
-        if (!$study) {
-            response()->json(['status' => 404,'message' => 'No se ha encontrado un estudio con ese id'], 404)->send();
-        die();
-    }
-}
-    */
+
 
     //Le metemos un middleware para que tenga que estar autenticado para continuar
     public function __construct()
@@ -37,7 +29,6 @@ class InstitutoController extends Controller
         $user = \Auth::user();
         if ($user->can("viewAny", Product::class)) {
 
-            // return Product::all(); Version incompleta. Version completa es devolver producto en json con un status
             $products = Product::all();
 
             return response()->json(['status' => 'ok', 'data' => $products], 200);
@@ -64,14 +55,6 @@ class InstitutoController extends Controller
      */
     public function store(Request $request)
     {
-        /*
-        //En la api el validate no valdría ya que manda datos, no va a redireccionar de nuevo al formulario si fallase
-        $request->validate([
-            "nombre"=>"required|max: 100",
-            "precio"=>"required|numeric|gt:0",
-            "descripcion"=>"required"
-        ]);*/
-
         //Para ello utilizamos Validator para validar los datos. Es un facade
         $validated = Validator::make($request->all(), [
             "nombre" => "required|max: 100",
@@ -82,8 +65,8 @@ class InstitutoController extends Controller
         if ($validated->fails()) {
             return response()->json(["status" => "NOK", "errors" => $validated->errors()], 422); //422 “unprocessable entity.”
         }
-        $newProduct = Instituto::create($request->all());
-        return response()->json(["status" => "ok", "data" => $newProduct], 201); //Cuando se crea algo se devuelve 201 - Created
+        $newInstituto = Instituto::create($request->all());
+        return response()->json(["status" => "ok", "data" => $newInstituto], 201); //Cuando se crea algo se devuelve 201 - Created
     }
 
     /**
@@ -94,18 +77,16 @@ class InstitutoController extends Controller
      */
     public function show($id)
     {
-        // Corresponde con la ruta /studies/{study}
-        // Buscamos un study por el ID.
-        $product = Instituto::find($id);
+        $instituto = Instituto::find($id);
 
-        // Chequeamos si encontró o no el product
-        if (!$product) {
+        // Chequeamos si encontró o no el instituto
+        if (!$instituto) {
             // Se devuelve un array errors con los errores detectados y código 404
-            return response()->json(['errors' => (['code' => 404, 'message' => 'No se encuentra un producto con ese código.'])], 404);
+            return response()->json(['errors' => (['code' => 404, 'message' => 'No se encuentra un instituto con ese código.'])], 404);
         }
 
         // Devolvemos la información encontrada.
-        return response()->json(['status' => 'ok', 'data' => $product], 200);
+        return response()->json(['status' => 'ok', 'data' => $instituto], 200);
     }
 
     /**
@@ -128,28 +109,7 @@ class InstitutoController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //Que busque el producto y de un error si no lo encuentra
-        $product = Instituto::find($id);
-        if (!$product) {
-            return response()->json(['status' => 'NOK', 'message' => 'No se encuentra un producto con ese código.'], 404);
-        }
-
-        //Validamos y creamos
-        $validated = Validator::make($request->all(), [
-            "nombre" => "required|max: 100",
-            "precio" => "required|numeric|gt:0",
-            "descripcion" => "required"
-        ]);
-
-        if ($validated->fails()) {
-            return response()->json(["status" => "NOK", "errors" => $validated->errors()], 422); //422 “unprocessable entity.”
-        }
-
-        //Obtenemos todos datos y los guardamos
-        $product->fill($request->all());
-        $product->save();
-
-        return response()->json(['status' => 'ok', 'data' => $product], 200);
+        //
     }
 
     /**
@@ -160,14 +120,14 @@ class InstitutoController extends Controller
      */
     public function destroy($id)
     {
-        $product = Instituto::find($id);
+        $instituto = Instituto::find($id);
 
-        if (!$product) {
-            return response()->json(['status' => 'NOK', 'message' => 'No se encuentra un producto con ese código.'], 404);
+        if (!$instituto) {
+            return response()->json(['status' => 'NOK', 'message' => 'No se encuentra un instituto con ese código.'], 404);
         }
 
         try {
-            $product->delete();
+            $instituto->delete();
             return response()->json(["mensaje" => "Borrado Correctamente"], 204);
         } catch (\Throwable $th) {
             return response()->json(["status" => "NOK", "mensaje" => "Borrado fallido", "error" => $th->getMessage()], 409);
